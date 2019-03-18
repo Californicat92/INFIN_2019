6abb7777b295d5f707562c27e4f46c14c8bf25c0
alumne <alumne@lae2001.(none)>
2019-03-18 18:22:44 +0100

    s'ha modificat el menu

--- menu/menu.c
+++ menu/menu.c
@@ -26,17 +26,7 @@
 *
 *
 */
-void ImprimirMenu(void)
-{
-	printf("\n\nMenu:\n");
-	printf("--------------------\n");
-	printf("1: Opció 1\n");
-	printf("2: Opció 2\n");
-	printf("3: Opció 3\n");
-	printf("s: Sortir\n");
-	printf("--------------------\n");
-}
-
+void ImprimirMenu(void);
 
 /************************
 *
@@ -54,32 +44,58 @@ int main(int argc, char **argv)
 
 	while (input != 's')
 	{
-		switch (input)
-		{
-			case '1':
-				printf("Heu seleccionat l'opció 1\n");	
-				ImprimirMenu();                             
-				break;
-			case '2':
-				printf("Heu seleccionat l'opció 2\n");	
-				ImprimirMenu();                             
-				break;
-			case '3':
-				printf("Heu seleccionat l'opció 3\n");	
-				ImprimirMenu();                             
-				break;
-			case 0x0a: //Això és per enviar els 0x0a (line feed) que s'envia quan li donem al Enter
-				break;
-			default:
-				printf("Opció incorrecta\n");	
-				printf("He llegit 0x%hhx \n",input);
-				break;
+		if (input > '~'){input=' ';}
+		{	
+			switch (input)
+			{
+				case '1':
+					printf("Heu seleccionat l'opció 1\n");	
+					ImprimirMenu();                             
+					break;
+				case '2':
+					printf("Heu seleccionat l'opció 2\n");	
+					ImprimirMenu();                             
+					break;
+				case '3':
+					printf("Heu seleccionat l'opció 3\n");	
+					ImprimirMenu();                             
+					break;
+				case '4':
+					printf("Heu seleccionat l'opció 4\n");	
+					ImprimirMenu();                             
+					break;
+				case '5':
+					printf("Heu seleccionat l'opció 5\n");	
+					ImprimirMenu();                             
+					break;
+				case '6':
+					printf("Heu seleccionat l'opció 6\n");	
+					ImprimirMenu();                             
+					break;
+				case 0x0a: //Això és per enviar els 0x0a (line feed) que s'envia quan li donem al Enter
+					break;
+				default:
+					printf("Opció incorrecta\n");	
+					printf("He llegit %c \n",input);
+					break;
+			}
 		}
-
-		input = getchar();
+			input = getchar();
 
 	}
 	
 	return 0;
+}
+void ImprimirMenu(void)
+{
+	printf("\n\nMenu:\n");
+	printf("-------------------------------------------------------\n");
+	printf("1.-Demanar última mitjana (promig del n-mostres anteriors)\n");
+	printf("2.-Demanar màxim\n");
+	printf("3.-Demanar mínim\n");
+	printf("4.-Reset màxim i mínim\n");
+	printf("5.-Demanar comptador\n");
+	printf("6.-Posar en marxa i parar adquisició\n");
+	printf("-------------------------------------------------------\n");
 }
 
